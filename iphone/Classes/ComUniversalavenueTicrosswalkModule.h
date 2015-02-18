b5c663b7962776ba69b35c051611eeb5943ea3e1
Jonatan Lundin <jonatan.lundin@gmail.com>
2015-02-18 08:33:55 +0100

    iOS module stub. Refs #1.

---
+++ iphone/Classes/ComUniversalavenueTicrosswalkModule.h
@@ -0,0 +1,14 @@
+/**
+ * TiCrosswalk
+ *
+ * Created by Jonatan Lundin
+ * Copyright (c) 2015 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface ComUniversalavenueTicrosswalkModule : TiModule
+{
+}
+
+@end
