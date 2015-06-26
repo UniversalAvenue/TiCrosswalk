020ccb0a4e77807bf8ca1a01a23ffd8b6ab983f7
Pyreweb <support@pyreweb.com>
2015-06-26 14:51:58 +0200

    Update index.js
    Added 2 buttons for navigation
    Cleaned up for Alloy simplification, but still have the non-alloy code.

--- xwtest/app/controllers/index.js
+++ xwtest/app/controllers/index.js
@@ -1,31 +1,75 @@
-var test = require('com.universalavenue.ticrosswalk');
-Ti.API.info("module is => " + test);
-Ti.API.info("module example() method returns => " + test.example());
-Ti.API.info("module exampleProp is => " + test.exampleProp);
-test.exampleProp = "This is a test value";
+/*** Alloy Example ***/
+var webView = $.webView;
+webView.setUrl('http://universalavenue.com');
 
-var webView = test.createWebView({ html: 'http://universalavenue.com' });
+/*** Classic Example ***
+var crossWalk = require('com.universalavenue.ticrosswalk'),
+    webView;
+Ti.API.info("module is => " + test);
+
+webView = crossWalk.createWebView({ html: 'http://universalavenue.com' });
 $.index.add(webView);
+/*** End Classic Example ***/
 
 webView.addEventListener('load', function (event) {
-  console.log('load!!', event);
+    console.log('load!!', event);
 });
 
-var move = Ti.UI.createButton({
-  left: 10, right: 10, bottom: 10, height: 44, title: 'move!'
-});
-move.addEventListener('click', function () {
-  webView.setUrl('http://google.se');
-});
-$.index.add(move);
+/*** Controls from external buttons ***/
+var pageChange,
+    pageNext,
+    pagePrev,
+    evalJS;
 
-var evaljs = Ti.UI.createButton({
-  left: 10, right: 10, bottom: 54, height: 44, title: 'evals!'
+pagePrev = Ti.UI.createButton({
+    left: 10,
+    bottom: 10,
+    height: 44,
+    width: '24%',
+    title: 'Prev'
 });
-evaljs.addEventListener('click', function () {
-  console.log('I can haz title:', webView.evalJS('document.title'));
+pagePrev.addEventListener('click', function () {
+    if (webView.canGoBack()) {
+        webView.goBack();
+    }
 });
-$.index.add(evaljs);
+$.index.add(pagePrev);
+pageChange = Ti.UI.createButton({
+    left: '25%',
+    right: 10,
+    bottom: 10,
+    height: 44,
+    width: '50%',
+    title: 'Go to Google.se!'
+});
+pageChange.addEventListener('click', function () {
+    webView.setUrl('http://google.se');
+});
+$.index.add(pageChange);
+pageNext = Ti.UI.createButton({
+    right: 10,
+    bottom: 10,
+    height: 44,
+    width: '24%',
+    title: 'Next'
+});
+pageNext.addEventListener('click', function () {
+    if (webView.canGoForward()) {
+        webView.goForward();
+    }
+});
+$.index.add(pageNext);
 
+evalJS = Ti.UI.createButton({
+    left: 10,
+    right: 10,
+    bottom: 54,
+    height: 44,
+    title: 'Send JS to webView!'
+});
+evalJS.addEventListener('click', function () {
+    console.log('I can haz title: ', webView.evalJS('document.title'));
+});
+$.index.add(evalJS);
 
 $.index.open();
